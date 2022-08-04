32669238c8de600eedd4eb9e762973545a3b9318
OwYeong <m.y.owyeong98@gmail.com>
2022-08-05 00:42:08 +0800

    first commit

---
+++ precache-manifest.7f82545bc976f63f489254c28a73bc8d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3ee6e95cf6f2b64c79a9ad65c1f4867",
+    "url": "/react-app-phonebook/index.html"
+  },
+  {
+    "revision": "446c020ea969f66e8903",
+    "url": "/react-app-phonebook/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "d33b55268fdd0ae80fd0",
+    "url": "/react-app-phonebook/static/css/main.7b1d56c8.chunk.css"
+  },
+  {
+    "revision": "446c020ea969f66e8903",
+    "url": "/react-app-phonebook/static/js/2.b16b9601.chunk.js"
+  },
+  {
+    "revision": "f9e7e68e90600aabdd7d1f759413461d",
+    "url": "/react-app-phonebook/static/js/2.b16b9601.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d33b55268fdd0ae80fd0",
+    "url": "/react-app-phonebook/static/js/main.bc886b4c.chunk.js"
+  },
+  {
+    "revision": "c8fa36cedf1a6ec48a05",
+    "url": "/react-app-phonebook/static/js/runtime-main.86e5ed6f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-app-phonebook/static/media/logo.5d5d9eef.svg"
+  }
+]);
